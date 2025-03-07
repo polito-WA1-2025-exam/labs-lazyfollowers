90854983e45a371a5b8643a2e03c272be93b8766
equisarque <guillaume@mantzarides.fr>
2025-03-07 12:06:11 +0100

    G adding method in order

--- entities/Order.mjs
+++ entities/Order.mjs
@@ -1,9 +1,12 @@
 "use strict"
 
 // constructor function of order 
-function Order (list_of_poke_bowl) {
-    poke_orders.push(this); //store the object in its container
-    this.total_price = () => pass // TO DO
+function Order () {
+    this.list_of_poke_bowl = [];
+
+    this.add_Poke = (Pokebowl) => this.list_of_poke_bowl.push(Pokebowl);
+    this.remove_Poke = (criteria, value) => this.list_of_poke_bowl.filter(Pokebowl => Pokebowl.criteria != value);
+    this.total_price = () => console.log("TO DO : calculate price");
 }
 
 
