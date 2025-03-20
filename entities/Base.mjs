8d577839ff6d8f170019d14e86e3419c248bab65
equisarque <guillaume@mantzarides.fr>
2025-03-20 14:13:57 +0100

    finish crud for base

--- entities/Base.mjs
+++ entities/Base.mjs
@@ -6,31 +6,46 @@ function Base(name) {
     this.name = name;
     // TODO: fetch by id, retrieve Id after insertion (maybe use let insertedId = this.lastID after run)
 
-    this.insert_into_database = () => {
-        let db = new DBconnection();
-        let stmtbase = db.db.prepare("INSERT INTO Bases (name) VALUES (?)");
-        stmtbase.run(this.name, function (err) {
-            if (err) {
-                throw err;
-            }
-            this.id = this.lastID;
-        });
-        stmtbase.finalize();
-        db.db.close();
-    }
-    this.update_database = async () => {
-        let db = new DBconnection();
-        if (this.id == undefined || this.name == undefined) {
-            throw new Error("id is not defined");
+    this.insert_base = () => {
+        return new Promise((resolve, reject) => {
+            let db = new DBconnection();
+            let stmtbase = db.db.prepare("INSERT INTO Bases (name) VALUES (?)");
+            stmtbase.run(this.name, function (err) {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve(this.lastID);
+                }
+            });
+            stmtbase.finalize();
+            db.db.close();
         }
-        let stmtbase = db.db.prepare("UPDATE INTO Bases SET (name) VALUES (?) WHERE id = ?");
-        stmtbase.run([this.name, this.id], function (err) {
-            if (err) {
-                throw err;
-            }
-        });
-        stmtbase.finalize();
-        db.db.close();
+        );
+    }
+    this.update_base = async () => {
+        return new Promise((resolve, reject) => {
+            let db = new DBconnection();
+            let stmtbase = db.db.prepare("UPDATE Bases SET (name) = (?) WHERE id = ?");
+            stmtbase.run(this.name, this.id, function (err) {
+                if (err) {
+                    if (this.id == undefined || this.name == undefined) {
+                        reject("id or name is not defined");
+                    }
+                    else {
+                        reject(err);
+                    }
+                }
+                let base_to_return = new Base();
+                base_to_return.id = this.id;
+                base_to_return.name = this.name;
+                resolve(base_to_return);
+            });
+            stmtbase.finalize();
+            db.db.close();
+        }
+        );
+
     }
     this.fetch_by_id = (id) => {
         return new Promise((resolve, reject) => {
--- index.mjs
+++ index.mjs
@@ -9,25 +9,25 @@ import Portion from "./entities/Portion.mjs";
 import OrderService from "./service/OrderService.mjs";
 import Base from "./entities/Base.mjs";
 
-let poke_1 = new PokeBowl('R');
 async function main() {
-    try {
-        let base = await new Base().fetch_by_id(7);
-        console.log(base);
-    }
-    catch (e) {
-        console.error(e);
-    }
+    // try {
+    //     let base2 = new Base('Inseré');
+    //     base2.id = await base2.insert_base(base2);
+    //     console.log(base2);
 
-    //     base.name = 'Pasta';
+    //     let base3 = await new Base().fetch_by_id(base2.id);
+    //     console.log(base3);
 
-    //     console.log(base);
-    //     try {
-    //         await base.update_database();
-    //     }
-    //     catch (e) {
-    //         console.error(e);
-    //     }
+    //     base3.name = 'Pasta';
+
+    //     let base4 = await base3.update_base();
+
+    //     console.log(base4);
+    //     console.log(base3);
+
+
+    // } catch (error) {
+    //     console.log(error);
+    // }
 }
-
 main();
